c9fb59c94723666262c95f38e8f599d656507e0b
Piotr Malecki <victorious.solutions@icloud.com>
2021-05-09 23:14:48 +0100

    Add router.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
+import Upload from './components/pages/Upload'
+import Gallery from './components/pages/Gallery'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Switch>
+          <Route path="/upload">
+            <Upload />
+          </Route>
+          <Route path="/">
+            <Gallery />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
