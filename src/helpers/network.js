513ebd2d26a132bbd0fd0d1168f3311942187186
Piotr Malecki <victorious.solutions@icloud.com>
2021-05-12 12:20:45 +0100

    Replace api key.

--- src/helpers/network.js
+++ src/helpers/network.js
@@ -1,7 +1,7 @@
 import queryString from "query-string"
 
 const CAT_API_URL = "https://api.thecatapi.com";
-const API_KEY = "3992842d-8a6c-4043-aa36-c18a4bf78c01";
+const API_KEY = "22b3467e-4c5d-4ad9-b5f8-923750d32c0d";
 
 export const GET = async ({ url, data = null, headers = {} }, callback) => {
   try {
@@ -19,33 +19,53 @@ export const GET = async ({ url, data = null, headers = {} }, callback) => {
       }
     })
 
+    const responseHeaders = res.headers;
+
     const jsonResponse = await res.json()
 
-    return res.ok ? callback(null, jsonResponse) : callback(jsonResponse, null)
+    return res.ok ? callback(null, jsonResponse, responseHeaders) : callback(jsonResponse, null)
   } catch (error) {
-    console.log({ error })
     callback({ message: "Request failed" })
   }
 }
 
 export const POST = async ({ url, data, headers = {} }, callback) => {
   try {
-    const res = await fetch(`${CAT_API_URL}\\${url}`, {
+    const res = await fetch(`${CAT_API_URL}${url}`, {
       method: 'POST',
-      mode: 'no-cors',
       headers: {
         'Access-Control-Allow-Origin': '*',
-        'Content-Type': 'application/json',
         'x-api-key': API_KEY,
         ...headers,
       },
-      body: JSON.stringify(data),
+      body: data,
     })
 
     const jsonResponse = await res.json()
 
     return res.ok ? callback(null, jsonResponse) : callback(jsonResponse, null)
   } catch (error) {
+    callback({ message: "Request failed" })
+  }
+}
+
+export const DELETE = async ({ url, headers = {} }, callback) => {
+  try {
+    const res = await fetch(`${CAT_API_URL}${url}`, {
+      method: 'DELETE',
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'application/json',
+        'x-api-key': API_KEY,
+        ...headers,
+      }
+    })
+
+    const jsonResponse = await res.json()
+
+    return res.ok ? callback(null, jsonResponse) : callback(jsonResponse, null)
+  } catch (error) {
+
     callback({ message: "Request failed" })
   }
 }
