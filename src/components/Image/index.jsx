16f227f4916e2add0503167ad9cd64a95ad675dc
Piotr Malecki <victorious.solutions@icloud.com>
2021-05-12 12:20:26 +0100

    Add image features.

--- src/components/Image/index.jsx
+++ src/components/Image/index.jsx
@@ -1,21 +1,79 @@
+import React, { useState, useEffect } from 'react';
 import GridListTile from '@material-ui/core/GridListTile';
 import GridListTileBar from '@material-ui/core/GridListTileBar';
 import IconButton from '@material-ui/core/IconButton';
 import StarBorderIcon from '@material-ui/icons/StarBorder';
+import StarIcon from '@material-ui/icons/Star';
+import { ThumbUp, ThumbDown } from '@material-ui/icons'
+import { useSelector, useDispatch } from 'react-redux';
+import { saveFavourite, removeFavourite, updateVote } from '../../actions/actions'
 import { useStyles } from './styles';
 
-const Image = ({ tile }) => {
+const Image = ({ image }) => {
   const classes = useStyles();
 
+  const favourites = useSelector(state => state.favourites.favourites)
+  const [favouriteID, setFavouriteID] = useState(null);
+
+  const votes = useSelector(state => state.votes.votes)
+  const [scores, setScores] = useState(0);
+
+  useEffect(() => {
+    const favouriteFound = favourites.find(item => item.image_id === image.id)
+    if (favouriteFound !== undefined) {
+      setFavouriteID(favouriteFound.id)
+    }
+  }, [favourites, image])
+
+  useEffect(() => {
+    const voteFound = votes.find(vote => vote.image_id === image.id)
+    if (voteFound !== undefined) {
+      setScores(voteFound.value)
+    }
+  }, [votes, image])
+
+
+  const dispatch = useDispatch();
+
+  const onClickedStar = (event) => {
+    if (favouriteID) {
+      dispatch(removeFavourite(favouriteID))
+      setFavouriteID(null)
+    } else {
+      dispatch(saveFavourite(image.id, (id) => { setFavouriteID(id) }))
+    }
+  }
+
+  const updateScores = (value) => {
+    setScores(value)
+    dispatch(updateVote({ image_id: image.id, value }))
+  }
+
+  const onUpVote = () => updateScores(scores + 1)
+
+  const onDownVote = () => updateScores(scores - 1)
+
   return (
-    <GridListTile key={tile.img} cols={tile.featured ? 2 : 1} rows={tile.featured ? 2 : 1}>
-      <img src={tile.img} alt={tile.title} />
+    <GridListTile key={image.img} cols={4} >
+      <img src={image.url} alt={image.url} className={classes.image} />
       <GridListTileBar
-        title={tile.title}
+        title={image.title}
         titlePosition="top"
-        actionIcon={<IconButton aria-label={`star ${tile.title}`} className={classes.icon}>
-          <StarBorderIcon />
-        </IconButton>}
+        actionIcon={
+          <div className={classes.actions}>
+            <IconButton onClick={onClickedStar} aria-label={`star ${image.title}`} className={classes.icon}>
+              {favouriteID ? <StarIcon /> : <StarBorderIcon />}
+            </IconButton>
+            <IconButton onClick={onUpVote} aria-label={`upvote ${image.title}`} className={classes.icon}>
+              <ThumbUp />
+            </IconButton>
+            <IconButton onClick={onDownVote} aria-label={`upvote ${image.title}`} className={classes.icon}>
+              <ThumbDown />
+            </IconButton>
+            <div className={classes.scores}>{scores}</div>
+          </div>
+
+        }
         actionPosition="left"
         className={classes.titleBar} />
     </GridListTile>
--- src/components/Image/styles.js
+++ src/components/Image/styles.js
@@ -1,11 +1,26 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 export const useStyles = makeStyles((theme) => ({
+  actions: {
+    display: 'flex'
+  },
   titleBar: {
     background: 'linear-gradient(to bottom, rgba(0,0,0,0.7) 0%, ' +
       'rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
   },
+  image: {
+    color: 'white',
+    height: 'auto',
+    maxWidth: "340px",
+    margin: '0 auto',
+  },
   icon: {
     color: 'white',
   },
+  scores: {
+    color: 'white',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
 }));
